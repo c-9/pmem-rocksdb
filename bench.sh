2574957c2ed982273c5b670dcf9bc8f3a479c2e1
chunk <chunk9@163.com>
2024-12-15 16:17:38 +0800

    benching procks

--- bench.sh
+++ bench.sh
@@ -7,9 +7,10 @@ export PMDK_INCLUDE_PATH=/usr/local/pmdk-2.1.0/include
 export PMDK_LIBRARY_PATH=/usr/local/pmdk-2.1.0/lib
 
 $current_dir/db_bench -benchmarks="fillrandom" \
-    -num=500 \
+    -threads=1 \
+    -num=100000 \
     -key_size=16 \
-    -value_size=67108864 \
+    -value_size=64 \
     -dcpmm_wal_enable=0 \
     -cache_index_and_filter_blocks_for_mmap_read=1 \
     -cache_data_blocks_for_mmap_read=0 \
